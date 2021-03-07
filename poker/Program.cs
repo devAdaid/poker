2fa63836bf3aedef06b03f2b05403f9ef877bf17
devAdaid <ahyeong2533@gmail.com>
2021-03-07 11:50:07 +0900

    VS 프로젝트 생성

---
+++ poker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Poker.Core;
+
+namespace Poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
